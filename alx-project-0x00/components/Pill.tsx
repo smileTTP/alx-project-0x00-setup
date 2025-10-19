e624ff272e7d5b37676b49934967b3b4691bf511
SmileTTP <ibtissamlazraf@gmail.com>
2025-10-19 16:57:46 +0100

    Rendering Components

--- alx-project-0x00/components/Pill.tsx
+++ alx-project-0x00/components/Pill.tsx
@@ -1,1 +1,10 @@
 
+const Pill: React.FC = () => {
+    return (
+        <div className=" flex justify-center  items-center bg-[#F9F9F9] px-3 w-auto h-[27px] rounded-full">
+            <p className=" text-sm ">Title</p>
+        </div>
+    )
+}
+
+export default Pill
